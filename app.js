610d605c70368baaa7318e367c4ee958c3f45b48
alfredo <alfredoaricoma@gmail.com>
2022-03-15 04:21:44 -0300

    agregue archivo gitignore nter the commit message for your changes. Lines starting

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 console.log ("hola mundo");
+// en la consola o preferentemente desde consola git bash tipear lo siguuiente:
+//git init.. comienza git y crea las carpetas ocultas
+//git status .. me muestra los archivos que tengo y no dentro en el proyecto
+// agrega el archivo al proyecto
+//$ git commit -- guarda la foto de ese momento con los archivos que estan dentro del proyecto
+// antes pide que guardemos nombre y mail del programador
+//git commit nuevamente
+//abre el editor bin.. aqui puedo dejar mensaje arriba y luego con telcla escape bajar teclear :wq  esto guarda el commit y le otorga un id para cada archivo
+//git log  -teclando esto me da un hash que permite diferenciar cada version de mis archivos comiiteados
+//git checkout --   este cmando incluendo ambos guiones me permite volver el archivo a la ultima foto del mismo guardado.. ya que si lo modifique con git status me figuta que el mismo archivo fue mofificado
+//quede 25´00´´
+// git diff index.html --- este comando me muestra las diferencias del ultimo snapshot guardado con el archivo actual..(index.html o el que le indique)
+//git add index.html --- para agregar los cambios al entorno de trabajo-- luego git comit index-- para guardarlos
+//.gitignore -- creamos esta carpeta dentro de la carpeta del proyecto para aqui escribir las carpetas archivos q vamos a ignorar de ser coomiteadas
+//luego agregar el archivo gitignore con -- git add.... luego comitear el archivo
+//
